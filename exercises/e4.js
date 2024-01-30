ab7a999c35b9422ffabc8a51a9bd4b000b461503
Alex <alexwimmer11@gmail.com>
2024-01-30 00:24:37 -0600

    answered all questions with a npm pass

--- exercises/e4.js
+++ exercises/e4.js
@@ -9,9 +9,8 @@
  * splitFirstAndLastNames('John Smith') => ['John', 'Smith']
  */
 
-function splitFirstAndLastNames(str) {
-  return str.split(' ');
-}
+
+const splitFirstAndLastNames = (str) => {return str.split(' ');}
 
 
 /** =========================
@@ -20,9 +19,7 @@ function splitFirstAndLastNames(str) {
  * personAge({ id: 1, name: 'Someone', age: 32}) => 32
  */
 
-const personAge = function(personObject) {
-  return personObject.age;
-}
+const personAge = personObject => {return personObject.age;}
 
 
 /** =========================
@@ -32,10 +29,7 @@ const personAge = function(personObject) {
  * isNameInArray(['Jon', 'Michael', 'Andrey'], 'James') => false;
  */
 
-const isNameInArray = function(arr, name) {
-  return arr.includes(name);
-}
-
+const isNameInArray = (arr, name) => {return arr.includes(name);}
 
 /** =========================
  * The logTimer function takes a number as the arguments.
@@ -44,9 +38,9 @@ const isNameInArray = function(arr, name) {
  * !!! Hint: There are two functions to refactor !!!
  */
 
-const logSecondsUpToMax = function(max) {
+const logSecondsUpToMax = (max) => {
   let i = 0;
-  const timer = setInterval(function() {
+  const timer = setInterval(() => {
     if (i < max) {
       console.log(++i);
     } else {
@@ -54,8 +48,6 @@ const logSecondsUpToMax = function(max) {
     }
   }, 1000);
 }
-
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-4"
